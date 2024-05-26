e9eaf08a04321a1be2710e51cd2fa9ac3c9c1c56
Rahul88187400 <psychorahul8818@gmail.com>
2024-05-26 12:54:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.get('/instagram',(req,res)=>{
+    res.send('jack.choudhary._');
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
